51f8eef9dd5ba17287e11ccc371bbc04f8f5c349
Pia Machado <piamachadocayetano1@gmail.com>
2022-03-28 16:46:48 -0300

    Agregué imágenes de proyectos

--- src/Components/Proyectos.jsx
+++ src/Components/Proyectos.jsx
@@ -1,26 +1,29 @@
 import Proyecto from './Proyecto';
 import './css/proyectos.css';
+import paseuy from '../images/paseuy.jpeg';
+import paseuyapp from '../images/paseuyapp.jpeg';
+import givt from '../images/givt.png';
 
 function Proyectos() {
   return (
     <section id="proyectos">
       <h2>Proyectos realizados</h2>
-      <div className='proyectosreal'>
-      <Proyecto
-        img=""
-        nombre="PaseUy"
-        descrip="Aplicación web dedicada a la compra y venta de entradas para diferentes eventos."
-      />
-      <Proyecto
-        img=""
-        nombre="PaseUy App"
-        descrip="Aplicación mobile desarrollada para validar entradas adquiridas en PaseUy, mediante reconocimiento de código QR."
-      />
-      <Proyecto
-        img=""
-        nombre="Givt"
-        descrip="Sitio web dedicado a la recaudación de dinero para diferentes eventos o causas, contribuyendo con ello a las ONGs asociadas."
-      />
+      <div className="proyectosreal">
+        <Proyecto
+          img={paseuy}
+          nombre="PaseUy"
+          descrip="Aplicación web dedicada a la compra y venta de entradas para diferentes eventos."
+        />
+        <Proyecto
+          img={paseuyapp}
+          nombre="PaseUy App"
+          descrip="Aplicación mobile desarrollada para validar entradas adquiridas en PaseUy, mediante reconocimiento de código QR."
+        />
+        <Proyecto
+          img=""
+          nombre={givt}
+          descrip="Sitio web dedicado a la recaudación de dinero para diferentes eventos o causas, contribuyendo con ello a las ONGs asociadas."
+        />
       </div>
     </section>
   );
