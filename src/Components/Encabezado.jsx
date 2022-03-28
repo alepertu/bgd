ed57d97000bd4fe23a92587796cd524b2be4d28e
Pia Machado <piamachadocayetano1@gmail.com>
2022-03-28 18:33:49 -0300

    importe las imagenes y el estado al hacer clic a las opciones

--- src/Components/Encabezado.jsx
+++ src/Components/Encabezado.jsx
@@ -1,16 +1,38 @@
 import './css/estiloencabezadob1.css';
 import logo from '../images/logobgdesarrollo.png';
+import React, { useState } from 'react';
 function Encabezado() {
+  let [opcion, setOpcion] = useState('inicio');
   return (
     <header>
       <div className="logo">
         <img src={logo} alt="Logo de BD Desarrollo"></img>
       </div>
       <nav>
-        <a href="#inicio">INICIO</a>
-        <a href="#nosotros">NOSOTROS</a>
-        <a href="#proyectos">PROYECTOS</a>
-        <a href="#contacto">CONTACTO</a>
+        <a
+          href="#inicio"
+          onClick={() => setOpcion('inicio')}
+          className={opcion == 'inicio' ? 'activa' : null}>
+          INICIO
+        </a>
+        <a
+          href="#nosotros"
+          onClick={() => setOpcion('nosotros')}
+          className={opcion == 'nosotros' ? 'activa' : null}>
+          NOSOTROS
+        </a>
+        <a
+          href="#proyectos"
+          onClick={() => setOpcion('proyectos')}
+          className={opcion == 'proyectos' ? 'activa' : null}>
+          PROYECTOS
+        </a>
+        <a
+          href="#contacto"
+          onClick={() => setOpcion('contacto')}
+          className={opcion == 'contacto' ? 'activa' : null}>
+          CONTACTO
+        </a>
       </nav>
     </header>
   );
