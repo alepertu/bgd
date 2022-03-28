380d9ecbebfd8fc5227b648cec88a331d48a0157
Pia Machado <piamachadocayetano1@gmail.com>
2022-03-28 16:26:24 -0300

    agregué las imágenes del Nosotros

--- src/Components/Nosotros.jsx
+++ src/Components/Nosotros.jsx
@@ -1,5 +1,9 @@
 import './css/nosotros.css';
 import Presentacion from './Presentacion';
+import ale from '../images/ale.jpeg';
+import moi from '../images/moi.jpeg';
+import moia from '../images/moia.jpeg';
+
 function Nosotros() {
   return (
     <section id="nosotros">
@@ -18,17 +22,17 @@ function Nosotros() {
       <Presentacion
         persona="Alejandro Pertusatti"
         bio="Soy Ale, tengo 23 años. Me recibí de Diseñador Gráfico en 2019, y soy un apasionado de la programación. Soy estudiante avanzado de Químico Farmacéutico en UdelaR. Además, trabajo actualmente en Customer Service de Mercado Libre."
-        perfil=""
+        perfil={moi}
       />
       <Presentacion
         persona="María Pía Machado"
         bio="Soy Pía, tengo 23 años. Me titulé como Analista en Programación en 2020. Actualmente soy estudiante avanzada de Ingeniería Química en UdelaR. Además de programar, trabajo en Customer Service de Mercado Libre. Distrufo de mis trabajos y de los momentos en familia."
-        perfil=""
+        perfil={moia}
       />
       <Presentacion
         persona="Alejandro Bustamante"
         bio="Soy Ale, tengo 25 años, me apasiona el mundo del emprendimiento, por lo que inicié mi primer proyecto en 2018, el cual cuenta hoy con más de 2mil usuarios, desde entonces me he dedicado al desarrollo y organización de diferentes empresas. Actualmente soy el director de la empresa Bustamante Gascue Group. Me encanta viajar y conocer las distintas formas en que puede vivir una persona en el mundo."
-        perfil=""
+        perfil={ale}
       />
       <section></section>
     </section>
--- src/Components/Proyecto.jsx
+++ src/Components/Proyecto.jsx
@@ -3,7 +3,7 @@ import './css/estiloproyectosb1.css';
 function Proyecto({ img, nombre, descrip }) {
   return (
     <div class="borde">
-      <img src={img} />
+      <img src={img} alt="Captura de pantalla del proyecto" />
       <h2>{nombre}</h2>
       <p>{descrip}</p>
     </div>
