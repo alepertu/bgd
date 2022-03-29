5aab78366ed2bdd1c1a1053444e3140ac5bf765d
Alejandro Pertusatti <alepertu@gmail.com>
2022-03-29 19:06:53 -0300

    Mejoras en la parte de proyectos

--- src/Components/Proyecto.jsx
+++ src/Components/Proyecto.jsx
@@ -1,10 +1,16 @@
 import './css/estiloproyectosb1.css';
+import external from '../images/iconos/external.svg';
 
-function Proyecto({ img, nombre, descrip }) {
+function Proyecto({ img, nombre, descrip, link }) {
   return (
     <div class="borde">
       <img src={img} alt="Captura de pantalla del proyecto" />
-      <h2>{nombre}</h2>
+      <h2>
+        {nombre}{' '}
+        <a href={link} target="_blank" rel="noreferrer">
+          <img src={external} alt="Ir al sitio" />
+        </a>
+      </h2>
       <p>{descrip}</p>
     </div>
   );
--- src/Components/Proyectos.jsx
+++ src/Components/Proyectos.jsx
@@ -13,16 +13,19 @@ function Proyectos() {
           img={paseuy}
           nombre="PaseUy"
           descrip="Aplicación web dedicada a la compra y venta de entradas para diferentes eventos."
+          link="https://paseuy.com/"
         />
         <Proyecto
           img={paseuyapp}
           nombre="PaseUy App"
           descrip="Aplicación mobile desarrollada para validar entradas adquiridas en PaseUy, mediante reconocimiento de código QR."
+          link="https://play.google.com/store/apps/details?id=com.bggroup.paseuy&hl=es"
         />
         <Proyecto
           img={givt}
           nombre="Givt"
           descrip="Sitio web dedicado a la recaudación de dinero para diferentes eventos o causas, contribuyendo con ello a las ONGs asociadas."
+          link="https://givt.uy/"
         />
       </div>
     </section>
