ff29bc91e8323048e103a7d20eab0490980b82a0
Alejandro Pertusatti <alepertu@gmail.com>
2024-11-17 12:46:11 -0300

    fix: main layout

--- app/_components/Section.tsx
+++ app/_components/Section.tsx
@@ -1,13 +1,15 @@
 export default function Section({
   id,
   children,
+  customizations,
 }: {
   id: string;
   children: React.ReactNode;
+  customizations?: string;
 }) {
   return (
     <section
-      className="text-center relative overflow-hidden w-full flex-col place-content-center h-lvh px-6 rounded-t-full"
+      className={`text-center relative overflow-hidden w-full h-lvh px-6 ${customizations}`}
       id={id}>
       {children}
     </section>
--- app/page.tsx
+++ app/page.tsx
@@ -15,38 +15,40 @@ function App() {
       <Header />
       <main>
         <Section id="inicio">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 480 480"
-            className="absolute top-36 -z-10 -left-8 rotate-90 fill-uy-yellow-200 w-3/12">
-            <path d="M360 240c-66.3 0-120-53.7-120-120a120 120 0 1 0-120 120c66.3 0 120 53.7 120 120a120 120 0 1 0 120-120Z"></path>
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 480 480"
-            className="absolute top-40 -z-10 rotate-12 -right-12 fill-uy-blue-50 w-3/12">
-            <path d="M240 0H0a240 240 0 0 0 240 240h240A240 240 0 0 0 240 0ZM240 240H0a240 240 0 0 0 240 240h240a240 240 0 0 0-240-240Z"></path>
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 480 480"
-            className="absolute top-72 -z-10 left-10 fill-uy-brown-50 w-3/12">
-            <path d="M197.6 42.4 42.4 197.6a60 60 0 0 0 0 84.8l155.2 155.2a60 60 0 0 0 84.8 0l155.2-155.2a60 60 0 0 0 0-84.8L282.4 42.4a60 60 0 0 0-84.8 0Z"></path>
-          </svg>
-          <h1 className="text-3xl font-bold px-8 tracking-tight">
-            Convertimos tu idea en realidad
-          </h1>
-          <p className="text-lg mt-6 mb-10 tracking-tight leading-6 text-balance">
-            Desarrollamos aplicaciones web para emprendedores y start-ups.{' '}
-            <b>Creamos tu producto digital desde cero</b> o mejoramos el que ya
-            tenés.
-          </p>
-          <a
-            className="bg-black py-2 px-4 text-xl rounded no-underline text-white font-semibold border-2 border-transparent hover:text-black transition-all hover:bg-transparent hover:border-black"
-            href="#contacto">
-            Contános tu idea
-          </a>
-          <div className="relative top-10 h-1/3">
+          <div className="relative h-1/2 flex flex-col place-content-end gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 480 480"
+              className="absolute top-36 -z-10 -left-8 rotate-90 fill-uy-yellow-200 w-3/12">
+              <path d="M360 240c-66.3 0-120-53.7-120-120a120 120 0 1 0-120 120c66.3 0 120 53.7 120 120a120 120 0 1 0 120-120Z"></path>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 480 480"
+              className="absolute top-40 -z-10 rotate-12 -right-12 fill-uy-blue-50 w-3/12">
+              <path d="M240 0H0a240 240 0 0 0 240 240h240A240 240 0 0 0 240 0ZM240 240H0a240 240 0 0 0 240 240h240a240 240 0 0 0-240-240Z"></path>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 480 480"
+              className="absolute top-72 -z-10 left-10 fill-uy-brown-50 w-3/12">
+              <path d="M197.6 42.4 42.4 197.6a60 60 0 0 0 0 84.8l155.2 155.2a60 60 0 0 0 84.8 0l155.2-155.2a60 60 0 0 0 0-84.8L282.4 42.4a60 60 0 0 0-84.8 0Z"></path>
+            </svg>
+            <h1 className="text-3xl font-bold px-8 tracking-tight">
+              Convertimos tu idea en realidad
+            </h1>
+            <p className="text-lg mt-6 mb-10 tracking-tight leading-6 text-balance">
+              Desarrollamos aplicaciones web para emprendedores y start-ups.{' '}
+              <b>Creamos tu producto digital desde cero</b> o mejoramos el que
+              ya tenés.
+            </p>
+            <a
+              className="bg-black py-2 px-4 text-xl rounded no-underline w-2/3 mx-auto text-white font-semibold border-2 border-transparent hover:text-black transition-all hover:bg-transparent hover:border-black"
+              href="#contacto">
+              Contános tu idea
+            </a>
+          </div>
+          <div className="relative h-1/2 top-10">
             <MainCard
               order={1}
               title="Tienda online"
@@ -65,7 +67,7 @@ function App() {
           </div>
           <span className="absolute bottom-0 left-0 w-full h-4 border-b-2 border-white bg-white rounded-t-full z-10"></span>
         </Section>
-        <Section id="nosotros">
+        <Section id="nosotros" customizations="rounded-t-full">
           <h2>¡Hola!</h2>
           <p>
             Somos una empresa uruguaya que busca instalarse en el mercado del
